ba72def0470064b2cb6f397d95c70d8efe2255f4
Fred Myers <fred@fred-myers.com>
2014-05-14 15:26:37 -0400

    Initial Commit

---
+++ BarCodeTest/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  BarCodeTest
+//
+//  Created by Fredrick Myers on 5/14/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BarCodeTest/FMViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMViewController.h
+//  BarCodeTest
+//
+//  Created by Fredrick Myers on 5/14/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMViewController : UIViewController
+
+@end
